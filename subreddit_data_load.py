abb479a7a596c6b12ec5d6985bbbbae85a966fe8
drew-walkerr <aandrewwalkerr@gmail.com>
2022-01-26 13:17:16 -0500

    completed aggregation of tsv files for pulling all subreddit posts

--- subreddit_data_load.py
+++ subreddit_data_load.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 import pathlib
 import os
-
+# For list of subreddits, make each subreddit name a path object we can paste into overall path
 subreddit_list = ['suboxone', 'methadone', 'opiates', 'OpiateRecovery']
 path_list = []
 for subreddit in subreddit_list:
     path = pathlib.Path(subreddit)
     path_list.append(path)
 
-#rootdir = pathlib.Path(r'C:\Users\awalk55\PycharmProjects\stigma_and_bias_suboxone_methadone\newsubs\methadone')
+#rootdir = os.path.join(r'INSERT SUBREDDIT FILE DIRECTORY HERE')
 data = []
 for subreddit in path_list:
     rootdir = os.path.join(r'C:\Users\awalk55\PycharmProjects\stigma_and_bias_suboxone_methadone\newsubs', subreddit)
@@ -19,5 +19,6 @@ for subreddit in path_list:
          print(file)
          df = pd.read_csv(file, sep='\t')
          data.append(df)
-bigdf = pd.concat(data)  # or pd.concat(data, axis='columns')
-# need to investigate nested for loops to make sure this iterates over each set of folders 
+    bigdf = pd.concat(data)
+
+bigdf.to_csv("all_subreddit_posts.csv")
