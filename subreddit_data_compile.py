9bf861fdbd49d195846c1e886ba9432597b25b1a
drew-walkerr <aandrewwalkerr@gmail.com>
2022-03-15 18:39:23 -0400

    added new subreddit data collection and compilation workflows

---
+++ subreddit_data_compile.py
@@ -0,0 +1,37 @@
+import glob
+import pandas as pd
+
+import pathlib
+import os
+# For list of subreddits, make each subreddit name a path object we can paste into overall path
+subreddit_list = ['suboxone', 'methadone', 'opiates', 'OpiateRecovery']
+path_list = []
+for subreddit in subreddit_list:
+    path = pathlib.Path(subreddit)
+    path_list.append(path)
+
+#rootdir = os.path.join(r'INSERT SUBREDDIT FILE DIRECTORY HERE')
+data = []
+for subreddit in path_list:
+    rootdir = os.path.join(r'C:\Users\awalk55\PycharmProjects\stigma_and_bias_suboxone_methadone\newsubs', subreddit)
+    rootdir = pathlib.Path(rootdir)
+        for file in rootdir.glob('*.tsv'):
+         print(file)
+         df = pd.read_csv(file, sep='\t')
+         data.append(df)
+    bigdf = pd.concat(data)
+
+bigdf.to_csv("all_subreddit_posts.csv")
+
+# Next step: use bias lexicon to search for any posts with mentions of lexicon words
+bias_big_df = bigdf[bigdf['body'].str.contains('abuser|junkie|alcoholic|drunk|habit|dirty|stigma|bias|stereotype|shame|blame', na=False)]
+# How to incorporate a Levenstein ratio match to "fuzzy join" these words
+# Validation?
+bias_big_df.to_csv("all_subreddit_bias_posts.csv")
+# Visualizations-- plotting frequencies over time, prevalences within each of the communities
+# https://www.datacamp.com/community/tutorials/fuzzy-string-python
+
+# for loop that goes through
+# for post in subreddit posts
+# if word in post is <= .9 Lev distance of any words in stigmawords
+# then append dataframe
---
+++ subreddit_data_search.py
@@ -0,0 +1,109 @@
+import glob
+import pandas as pd
+
+import pathlib
+import os
+import re
+# ftfy -- amazing text cleaning for decode issues..
+from ftfy import fix_text
+import numpy as np
+from scipy.sparse import csr_matrix
+import sparse_dot_topn.sparse_dot_topn as ct
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sparse_dot_topn import awesome_cossim_topn
+subreddits = pd.read_csv("all_subreddit_posts.csv")
+# 172,375 posts /comments
+# Which of these are comments vs posts?
+# those which paren_comment_id = "nan"
+# body = text fields of interest
+
+# fast text queries
+# https://towardsdatascience.com/fuzzy-matching-at-scale-84f2bfd0c536
+
+
+def ngrams(string, n=3):
+    string = str(string)
+    string = fix_text(string) # fix text
+    string = string.encode("ascii", errors="ignore").decode() #remove non ascii chars
+    string = string.lower()
+    chars_to_remove = [")","(",".","|","[","]","{","}","'"]
+    rx = '[' + re.escape(''.join(chars_to_remove)) + ']'
+    string = re.sub(rx, '', string)
+    string = string.replace('&', 'and')
+    string = string.replace(',', ' ')
+    string = string.replace('-', ' ')
+    string = string.title() # normalise case - capital at start of each word
+    string = re.sub(' +',' ',string).strip() # get rid of multiple spaces and replace with a single
+    string = ' '+ string +' ' # pad names for ngrams...
+    string = re.sub(r'[,-./]|\sBD',r'', string)
+    ngrams = zip(*[string[i:] for i in range(n)])
+    return [''.join(ngram) for ngram in ngrams]
+
+
+def awesome_cossim_top(A, B, ntop, lower_bound=0):
+    # force A and B as a CSR matrix.
+    # If they have already been CSR, there is no overhead
+    A = A.tocsr()
+    B = B.tocsr()
+    M, _ = A.shape
+    _, N = B.shape
+
+    idx_dtype = np.int32
+
+    nnz_max = M * ntop
+
+    indptr = np.zeros(M + 1, dtype=idx_dtype)
+    indices = np.zeros(nnz_max, dtype=idx_dtype)
+    data = np.zeros(nnz_max, dtype=A.dtype)
+
+    ct.sparse_dot_topn(
+        M, N, np.asarray(A.indptr, dtype=idx_dtype),
+        np.asarray(A.indices, dtype=idx_dtype),
+        A.data,
+        np.asarray(B.indptr, dtype=idx_dtype),
+        np.asarray(B.indices, dtype=idx_dtype),
+        B.data,
+        ntop,
+        lower_bound,
+        indptr, indices, data)
+
+    return csr_matrix((data, indices, indptr), shape=(M, N))
+
+subreddit_text = subreddits['body']
+vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams)
+tf_idf_matrix = vectorizer.fit_transform(subreddit_text)
+
+import time
+t1 = time.time()
+matches = awesome_cossim_topn(tf_idf_matrix, tf_idf_matrix.transpose(),10, 0.85)
+t = time.time()-t1
+print("SELFTIMED:", t)
+
+
+def get_matches_df(sparse_matrix, name_vector, top=100):
+    non_zeros = sparse_matrix.nonzero()
+
+    sparserows = non_zeros[0]
+    sparsecols = non_zeros[1]
+
+    if top:
+        nr_matches = top
+    else:
+        nr_matches = sparsecols.size
+
+    left_side = np.empty([nr_matches], dtype=object)
+    right_side = np.empty([nr_matches], dtype=object)
+    similairity = np.zeros(nr_matches)
+
+    for index in range(0, nr_matches):
+        left_side[index] = name_vector[sparserows[index]]
+        right_side[index] = name_vector[sparsecols[index]]
+        similairity[index] = sparse_matrix.data[index]
+
+    return pd.DataFrame({'left_side': left_side,
+                         'right_side': right_side,
+                         'similairity': similairity})
+
+stigma_words = ["abuser","junkie","alcoholic","drunk","habit","dirty","stigma","bias","stereotype","shame","blame"]
+matches_df = get_matches_df(matches, org_names, top=1000)
+matches_df.sample(20)
