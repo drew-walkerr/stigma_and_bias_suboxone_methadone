2aa67409d309dc81f6059063f1fc5885bd9c8269
drew-walkerr <aandrewwalkerr@gmail.com>
2022-04-01 11:47:49 -0400

    added updated word list

--- subreddit_data_load.py
+++ subreddit_data_load.py
@@ -24,7 +24,17 @@ for subreddit in path_list:
 bigdf.to_csv("all_subreddit_posts.csv")
 
 # Next step: use bias lexicon to search for any posts with mentions of lexicon words
-bias_big_df = bigdf[bigdf['body'].str.contains('user|abuser|junkie|alcoholic|drunk|habit|dirty|stigma|bias|stereotype|shame|blame', na=False)]
-
+# Read in bias words, separate by |
+bias_words = pd.read_csv("word_list_expanded.csv")
+bias_words["stem_word"]
+bias_big_df = bigdf[bigdf['body'].str.contains('abuser|junkie|alcoholic|drunk|habit|dirty|stigma|bias|stereotype|shame|blame', na=False)]
+# How to incorporate a Levenstein ratio match to "fuzzy join" these words
+# Validation?
+bias_big_df.to_csv("all_subreddit_bias_posts.csv")
 # Visualizations-- plotting frequencies over time, prevalences within each of the communities
+# https://www.datacamp.com/community/tutorials/fuzzy-string-python
 
+# for loop that goes through
+# for post in subreddit posts
+# if word in post is <= .9 Lev distance of any words in stigmawords
+# then append dataframe
